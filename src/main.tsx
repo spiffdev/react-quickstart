ad7e558433ab09010f8ad1894c49093bc18e4d7a
Joshua Jones <joshuajones2010@gmail.com>
2023-06-23 13:31:04 +1000

    Add prettier, bug fixes (#4)
    * Add prettier as dev dependency
    
    * Remove unnecessary call to init client
    
    * Remove strict mode rendering

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,5 @@ const integrationProduct = '5141150b-8419-4e24-ae3f-9cab47a7920f'; // Sample Ser
 const workflowID = '3b09df2b-8808-4b1c-955a-d4172e706d11'; // Sample Serving Board Workflow
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <App integrationProductId={integrationProduct} workflowId={workflowID} />
-    </React.StrictMode>,
+    <App integrationProductId={integrationProduct} workflowId={workflowID} />,
 );
